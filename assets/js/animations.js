0e71b4d33eda0c6ae44ed8e05357a0487ea743f8
Niranjan Ravichandran <niranjanravichandran@live.com>
2015-12-14 12:27:22 -0500

    Code refinement
    Code changes and bug fixes

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,9 +1,17 @@
 //Home page typed animation
 $(document).ready(function(){
+
   $('.typed-content').typed({
     strings:["a Designer^500","a Developer.^500", "Niranjan Ravichandran.^1000"],
     typeSpeed: 100,
     loop: true,
     loopCount: 3,
+  });
+  $('.inner h2').hide();
+  $(window).scroll(function(event){
+    var y = $(this).scrollTop();
+    if (y > 2000){
+      $('.inner h2').show(2000);
+    }
   });
 });
